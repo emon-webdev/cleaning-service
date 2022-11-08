e11276d7536b7b308f61065d8da1ade76903be77
emon-webdev <emon.webdev@gmail.com>
2022-11-08 14:16:04 +0600

    phonto view add

--- src/components/SingleService.js
+++ src/components/SingleService.js
@@ -1,13 +1,20 @@
 import React from "react";
+import { PhotoProvider, PhotoView } from "react-photo-view";
+import "react-photo-view/dist/react-photo-view.css";
 import { Link } from "react-router-dom";
-
 const SingleService = ({ services }) => {
-  const {  title,_id, price, about, picture, rating } = services;
+  const { title, _id, price, about, picture, rating } = services;
   return (
     <div className="single-service">
-      <img src={picture} alt="" srcSet="" />
+      <PhotoProvider>
+        <PhotoView src={picture}>
+          <img src={picture} alt="" />
+        </PhotoView>
+      </PhotoProvider>
       <div className="p-5">
-        <h2 className="text-2xl font-semibold text-[#1039AD]">{services?.name}</h2>
+        <h2 className="text-2xl font-semibold text-[#1039AD]">
+          {services?.name}
+        </h2>
         <h3>{title}</h3>
         <p>{about.slice(0, 70) + "..."}</p>
         <p className="text-[#1039AD] my-2">Price: ${price}</p>
